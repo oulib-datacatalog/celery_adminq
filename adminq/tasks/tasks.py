99fc7de315f5cf3711b962fe23b26f67bee472b7
tdpearson <tylerdpearson@gmail.com>
2017-03-24 16:08:58 -0500

    commands need to be in list

--- adminq/tasks/tasks.py
+++ adminq/tasks/tasks.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO)
 def _call_systemctl(action):
     if action in ['start', 'stop', 'restart']:
         try:
-            check_call("sudo", "/usr/bin/systemctl", action, "oulib-celery-workerq")
+            check_call(["sudo", "/usr/bin/systemctl", action, "oulib-celery-workerq"])
         except CalledProcessError as err:
             logging.error(err)
             return({"ERROR": err})
